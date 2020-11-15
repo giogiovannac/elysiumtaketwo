6b51f335aa6dcf559cabd2997b51a19850a50704
Giovanna Coral <giovannacoralb@yahoo.com>
2020-11-14 20:11:02 -0500

    i am testing again

--- app/src/main/java/com/example/elysiumtaketwo/MainActivity.kt
+++ app/src/main/java/com/example/elysiumtaketwo/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-
-    // i am testing again
-
+    fun function(){
+        // this is a test
+    }
 }
